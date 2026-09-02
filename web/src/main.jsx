8b0291ec69e4ad0f6230debf3bb463f183e8f2b6
faris11z <sulaiman11faris@gmail.com>
2026-09-02 21:26:16 +0200

    fix: set React Router basename for GitHub Pages subpath
    All navigation now correctly targets /aviation_weather_briefing_bot/* paths.
    Previously clicking Home navigated to root / (404), Dashboard to /dashboard (404).

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/aviation_weather_briefing_bot">
       <App />
     </BrowserRouter>
   </React.StrictMode>
